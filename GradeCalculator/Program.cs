7e0533edd8ecee2a5247ba51ab4b5b9aa6075b5a
hydi-official <esseldacosta00@gmail.com>
2024-06-01 20:03:36 +0000

    Ticket Calculator Successfully Implemented

--- GradeCalculator/Program.cs
+++ GradeCalculator/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class GradeCalculator
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to my Grade Calculator Application");
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {userName}!");
+
+        Console.WriteLine("Enter your numerical grade (between 0 and 100):");
+        int grade = Convert.ToInt32(Console.ReadLine());
+
+        GradeConverter converter = new GradeConverter();
+        string letterGrade = converter.ConvertToLetterGrade(grade);
+
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+    }
+}
+
+class GradeConverter
+{
+    public string ConvertToLetterGrade(int grade)
+    {
+        if (grade >= 90 && grade <= 100)
+        {
+            return "A";
+        }
+        else if (grade >= 80 && grade <= 89)
+        {
+            return "B";
+        }
+        else if (grade >= 70 && grade <= 79)
+        {
+            return "C";
+        }
+        else if (grade >= 60 && grade <= 69)
+        {
+            return "D";
+        }
+        else if (grade >= 0 && grade < 60)
+        {
+            return "F";
+        }
+        else
+        {
+            return "Invalid grade entered.";
+        }
+    }
+}
---
+++ TicketCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class TicketCalculator
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Movie Theater Ticket Calculator");
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {userName}!");
+
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        double ticketPrice = CalculateTicketPrice(age);
+
+        Console.WriteLine($"The ticket price for you is GHC {ticketPrice:F2}");
+    }
+
+    public static double CalculateTicketPrice(int age)
+    {
+        const double standardPrice = 10;
+        const double discountedPrice = 7;
+
+        if (age >= 65 || age <= 12)
+        {
+            return discountedPrice;
+        }
+        else
+        {
+            return standardPrice;
+        }
+    }
+}
